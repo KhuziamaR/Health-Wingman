683e62bd4bb92f667efb8cafeae404cd78c7856f
Khuziama <rehmankhuziama@gmail.com>
2021-05-17 22:43:53 -0400

    Completed Main application features

---
+++ app/src/main/java/com/example/healthwingman/BmrActivity.java
@@ -0,0 +1,14 @@
+package com.example.healthwingman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BmrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bmr);
+    }
+}
---
+++ app/src/main/java/com/example/healthwingman/MyApplication.java
@@ -0,0 +1,2 @@
+package com.example.healthwingman;public class MyApplication {
+}
